6dad58ee9f091f2b5fd9241a1c3183b85d01410a
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2023-02-02 15:12:21 -0300

    update: searchUpdate

--- src/controllers/pos.controller.js
+++ src/controllers/pos.controller.js
@@ -1,6 +1,7 @@
 const { Op } = require('sequelize');
-const { ticketsl_promo } = require('../models');
+const { ticketsl_promo, ticketsl_loja } = require('../models');
 const { SHA256, AES } = require('crypto-js');
+
 const {
     tbl_pos,
     tbl_pdvs,
@@ -11,6 +12,12 @@ const {
     tbl_itens_classes_ingressos,
     tbl_classes_ingressos_pdvs
 } = ticketsl_promo;
+
+const {
+    lltckt_category,
+    lltckt_eve_categorias
+} = ticketsl_loja;
+
 
 /**
  * Controlador das maquininhas (POS)
@@ -218,20 +225,29 @@ class POSController {
                 where: {
                     eve_cod: { [Op.in]: allowed_eventos }
                 },
-                include: {
-                    model: tbl_classes_ingressos,
-                    where: {
-                        cla_cod: { [Op.in]: allowed_tickets }
+                include: [
+                    {
+                        model: lltckt_eve_categorias,
+                        include: {
+                            model: lltckt_category
+                        },
+                        limit: 1//
                     },
-                    include: {
-                        model: tbl_itens_classes_ingressos,
-                        order: [
-                            ['itc_prioridade', 'ASC'], // organizar por prioridade
-                            ['itc_quantidade', 'ASC']  //  "    "   por quantidade
-                        ],
-                        limit: 1
+                    {
+                        model: tbl_classes_ingressos,
+                        where: {
+                            cla_cod: { [Op.in]: allowed_tickets }
+                        },
+                        include: {
+                            model: tbl_itens_classes_ingressos,
+                            order: [
+                                ['itc_prioridade', 'ASC'], // organizar por prioridade
+                                ['itc_quantidade', 'ASC']  //  "    "   por quantidade
+                            ],
+                            limit: 1
+                        }
                     }
-                }
+                ]
             })
             .then(eventos => {
                 const data = JSON.parse(JSON.stringify(eventos));
@@ -254,17 +270,38 @@ class POSController {
                     // Organiza os dados (eventos)
                     data.forEach(value => {
                         setChanges.push(new Promise(async (resolve, _) => {
+                            /* Logo do Evento */
                             // URL base das imagens dos eventos
-                            const url_base = 'https://qingressos.com/image/cache/data';
+                            const url_base = 'https://qingressos.com/image/cache';
+
+                            // Endereço da imagem
+                            const image = value.lltckt_eve_categorias[0].lltckt_category.imageOutdoor;
 
                             // Link da imagem do evento
-                            value.image_logo = `${url_base}/${value.eve_cod}/${value.eve_cod}_imageOutdor-457x400.png`;
+                            value.image_logo = `${url_base}/${image}`;
 
+                            // Obtêm a extensão da imagem
+                            const type = [
+                                value.image_logo.indexOf('.png'),
+                                value.image_logo.indexOf('.jpg'),
+                            ]
+                            
+                            // Adiciona as dimensões da imagem
+                            const position = type.find(a => a >= 0);
+                            value.image_logo = value.image_logo.substring(0, position) + '-457x400' + value.image_logo.substring(position);
+
+
+                            /* Alterações no json */
                             // Altera "tbl_itens_classes_ingressos" de um array[1] para json
                             value.tbl_classes_ingressos.map(a => {
                                 a.tbl_itens_classes_ingressos = a.tbl_itens_classes_ingressos[0];
                                 return a;
                             });
+
+                            // Altera "lltckt_eve_categorias" de um array para json
+                            // obs.: pegando o primeiro valor do array (mudar depois)
+                            value.lltckt_category = value.lltckt_eve_categorias[0].lltckt_category;
+                            delete value.lltckt_eve_categorias;
     
                             resolve(value);
                         }));
